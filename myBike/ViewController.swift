cbee6af0be3004223aea0d9164b65ee2e0b2a1ef
Todd Judd <todd.judd@gmail.com>
2017-04-09 16:28:18 -0600

    Initial Commit

---
+++ myBike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myBike
+//
+//  Created by David T Judd on 4/9/17.
+//  Copyright © 2017 Tracer Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
